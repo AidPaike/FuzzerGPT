942375eac950fa8d726ad881b4e18f152cbcd651
aidpaike <f335125303@163.com>
2023-08-09 20:46:52 +0800

    database operate

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
+import asyncio, time
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain, SequentialChain
 from langchain.agents import initialize_agent, AgentType, Tool
 from langchain.prompts import ChatPromptTemplate
 from src.fuzz_utils.tool_crawler import get_bugreport
 from src.fuzz_utils.tool_prompt import get_generate_template
+from utils.db_connect import MongoDBHandle
 
 # 在conf中设置了代理和密钥
 from conf import fuzzgpt_globals
@@ -20,8 +22,14 @@ import 推荐方法
 '''
 
 if __name__ == '__main__':
+    # 初始化数据库
+    mongodb_connection = MongoDBHandle()
+
     # 定义llm
     llm = ChatOpenAI(temperature=0)
+
+    # 定义ids
+    ids = ["8212070", "8212070"]
 
     generate_chain = LLMChain(llm=llm, prompt=get_generate_template())
 
@@ -36,11 +44,20 @@ if __name__ == '__main__':
         llm,
         agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION,
         handle_parsing_errors=True,
-        verbose=False)
+        verbose=True)
     try:
+        # s = time.perf_counter()
+        # tasks = [agent.arun(q) for q in ids]
+        # await asyncio.gather(*tasks)
+        # elapsed = time.perf_counter() - s
+        # print(f"Concurrent executed in {elapsed:0.2f} seconds.")
         result = agent(
             "please get https://bugs.java.com/bugdatabase/view_bug?bug_id=8212070 content and generate test case based on this report"
         )
         print(result)
+        print(type(result))
+        one_return = mongodb_connection.insert_code_one(result)
+        mongodb_connection.close()
+        print("已存入数据库，请查看")
     except:
         print("exception on external access")
--- utils/db_connect.py
+++ utils/db_connect.py
@@ -1,23 +1,33 @@
 from pymongo import MongoClient
 
 
-class MongoDBConn:
+class MongoDBHandle(object):
+    """mongodb operate"""
 
-    def __init__(self, host, port, db_name, db_collection, username, password):
+    def __init__(self):
         """
         建立数据库连接
         """
-        # self.client = MongoClient(f"mongodb://{host}:{port}/")
-        self.client = MongoClient(host, port, username=f"{username}", password=f"{password}")
-        self.db = self.client[f"{db_name}"]
-        self.collection = self.db[f"{db_collection}"]
+        self.config = dict(host="127.0.0.1",
+                           port=27017,
+                           db_name="version1",
+                           db_collection="mydb",
+                           username="admin",
+                           password="123456")
+        self.client = MongoClient(self.config["host"], self.config["port"], username=f"{self.config['username']}",
+                                  password=f"{self.config['password']}")
+        self.db = self.client[f"{self.config['db_name']}"]
+        self.collection = self.db[f"{self.config['db_name']}"]
+
+    def close(self):
+        self.client.close()
 
     def insert_code_one(self, content):
         mydict = {
-            "prompt": content["prompt"],
-            "code": content["code"],
-            "url": content["url"],
-            "time": content["time"]
+            "input": content["input"],
+            "output": content["output"],
+            # "url": content["url"],
+            # "time": content["time"]
         }
 
         ok = self.collection.insert_one(mydict)
@@ -34,13 +44,12 @@ class MongoDBConn:
 
 
 if __name__ == '__main__':
-    mongodb_connection = MongoDBConn(host="127.0.0.1",
-                                     port=27017,
-                                     db_name="test",
-                                     db_collection="mydb",
-                                     username="aidpaike",
-                                     password="123456")
+    mongodb_connection = MongoDBHandle()
     dblist = mongodb_connection.client.list_database_names()
     collection_list = mongodb_connection.collection.find()
     print(dblist)
     print([x for x in collection_list])
+    one = {"prompt": "Taobao", "code": "100", "url": "https://www.taobao.com", "time": ""}
+    one_return = mongodb_connection.insert_code_one(one)
+    print(one_return)
+    mongodb_connection.close()
